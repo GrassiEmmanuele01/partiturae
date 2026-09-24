1689920a4c3fe94a357e3ab46d695eb91d9fca69
GrassiEmmanuele <emmanuele.grassi@mat.tn.it>
2026-09-24 10:09:59 +0200

    chore: initial project setup (Spring Boot + MySQL via Docker)

---
+++ backend/src/test/java/com/grassi/partiturae/PartituraeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grassi.partiturae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartituraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
